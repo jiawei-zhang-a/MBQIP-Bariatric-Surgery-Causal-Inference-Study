908e2387f48d552a0f6e1d18518ad49fa581e56b
jiawei-zhang-a <jz4721@nyu.edu>
2023-05-18 22:37:23 -0400

    fix the problem of orthoforest

--- EconML/ORF/OrthoForest_BMI.py
+++ EconML/ORF/OrthoForest_BMI.py
@@ -1,14 +1,25 @@
+from sklearn.ensemble import RandomForestRegressor
 import sys
 sys.path.append('../../utils')
 import mbqip_read_run as mbqip_utils
-import numpy as np
 from econml.orf import DROrthoForest
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestClassifier
+
 
 def main():
-    PATH = "/scratch/jz4721/Observational-Study/data/mbqip/csv/BMI"
-    est = DROrthoForest(model_Y = RandomForestRegressor())
+    PATH = "/scratch/jz4721/Observational-Study"
+    est = DROrthoForest(propensity_model = RandomForestClassifier(),
+                            model_Y = RandomForestRegressor(),
+                        propensity_model_final = RandomForestClassifier(),
+                         model_Y_final = RandomForestRegressor()
+                        )
     print(mbqip_utils.run_mbqip(est,PATH))
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
