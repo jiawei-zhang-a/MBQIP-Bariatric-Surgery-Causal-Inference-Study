ecc41443b126747837a6b3d74444b0b22ec801ca
jiawei-zhang-a <jz4721@nyu.edu>
2023-09-02 21:55:42 -0400

    MAINT

--- EconML/Meta-Learners/Meta-Learners_BMI.py
+++ EconML/Meta-Learners/Meta-Learners_BMI.py
@@ -8,9 +8,9 @@ import mbqip_read_run as mbqip_utils
 def main():
     print("\list \n(1)RYGB\n(2)Band\n(3)BPD-DS\n(4)SADI-S \BMI treatment effect\n")
 
-    print("SLearner")
-    est = SLearner(overall_model=RandomForestRegressor())
-    print(mbqip_utils.run_mbqip(est,"/scratch/jz4721/Observational-Study/data/mbqip/csv/BMI"))
+    #print("SLearner")
+    #est = SLearner(overall_model=RandomForestRegressor())
+    #print(mbqip_utils.run_mbqip(est,"/scratch/jz4721/Observational-Study/data/mbqip/csv/BMI"))
 
     print("TLearner")
     est = TLearner(models=RandomForestRegressor())
