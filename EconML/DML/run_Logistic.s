51da416e7b18932c0a2f244b6b6e39247105ce8b
jiawei-zhang-a <jz4721@nyu.edu>
2023-09-21 23:46:33 -0400

    MIANT

--- EconML/DML/run_Logistic.s
+++ EconML/DML/run_Logistic.s
@@ -19,5 +19,5 @@ export PATH=/scratch/jz4721/Observational-Study/venv/lib64/python3.8/bin:$PATH
 source ~/.bashrc
 
 cd EconML/DML
-python CausalForest_Logistic.py $SLURM_ARRAY_TASK_ID
+python CausalForest_Logistic.py 
 
