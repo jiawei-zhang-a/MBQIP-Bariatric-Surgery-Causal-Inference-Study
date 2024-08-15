6b7f48626671d8e28955840adf398bbc72566ac7
Qishuo Yin <qy1448@princeton.edu>
2024-08-15 17:19:56 -0400

    Add files via upload

---
+++ Final/effect/effectCausalForest.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=CausalDML_BMI
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=28
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=50G
+#SBATCH --time=04:00:00
+#SBATCH --output=effectCausalForest.out
+#SBATCH --error=effectCausalForest.err
+#SBATCH --mail-type=END
+#SBATCH --mail-user=jz4721@nyu.edu
+
+
+export OMP_NUM_THREADS=1
+
+module purge
+
+singularity exec --nv \
+    --overlay /scratch/jz4721/pyenv/overlay-15GB-500K.ext3:ro \
+    /scratch/work/public/singularity/cuda11.6.124-cudnn8.4.0.27-devel-ubuntu20.04.4.sif \
+    /bin/bash -c "source /ext3/env.sh; python /scratch/jz4721/SCI/EconML/CausalForestDML/effectCausalForest.py"
