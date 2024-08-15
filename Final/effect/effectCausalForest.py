d4c536a3c93a6ad4f7d24b223e7b1dbbae7e6ed1
Qishuo Yin <qy1448@princeton.edu>
2024-08-15 17:46:34 -0400

    Update effectCausalForest.py

--- Final/effect/effectCausalForest.py
+++ Final/effect/effectCausalForest.py
@@ -10,30 +10,15 @@ import numpy as np
 np.random.seed(0)
 
 def main():
-    print("\list \n(1)RYGB\n(2)Band\n(3)BPD-DS\n(4)SADI-S \BMI treatment effect\n")
     
-    # model propensity and outcome by random forest
     est = CausalForestDML(model_y=RandomForestRegressor(),
-                        model_t=RandomForestRegressor(),
+                        model_t=RandomForestRegressor(), # model propensity and outcome by random forest
                         criterion='mse', n_estimators=1000,
                         min_impurity_decrease=0.001,
                         )
+    
+    print("\list \n(1)RYGB\n(2)Band\n(3)BPD-DS\n(4)SADI-S \BMI treatment effect\n")
     print("CausalForestDML with random forest")                  
-    print(mbqip_utils.run_mbqip(est,PATH + "data/mbqip/csv/BMI"))
-
-    # model propensity and outcome by gradient boosting
-    est = CausalForestDML(model_y=lgb.LGBMRegressor(verbosity = -1),
-                        model_t=lgb.LGBMRegressor(verbosity = -1),
-    )
-
-    print("CausalForestDML with lightgbm")
-    print(mbqip_utils.run_mbqip(est,PATH + "data/mbqip/csv/BMI"))
-
-    # model propensity and outcome by XGBoost
-    est = CausalForestDML(model_y=xgb.XGBRegressor(),
-                        model_t=xgb.XGBRegressor(),
-    )
-    print("CausalForestDML with xgboost")
     print(mbqip_utils.run_mbqip(est,PATH + "data/mbqip/csv/BMI"))
 
 if __name__ == '__main__':
