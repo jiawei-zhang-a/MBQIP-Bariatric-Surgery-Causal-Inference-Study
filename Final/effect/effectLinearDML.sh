54e66ebcf303a765da1121503c2e55a2dec8e0ac
Qishuo Yin <qy1448@princeton.edu>
2024-08-15 18:05:51 -0400

    Update and rename run_BMI.s to effectLinearDML.sh

--- Final/effect/effectLinearDML.sh
+++ Final/effect/effectLinearDML.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=DML_BMI
+#SBATCH --job-name=effectLinearDML
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=28
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=50G
 #SBATCH --time=11:00:00
-#SBATCH --output=BMI.out
-#SBATCH --error=BMI.err
+#SBATCH --output=effectLinearDML.out
+#SBATCH --error=effectLinearDML.err
 #SBATCH --mail-type=END
 #SBATCH --mail-user=jz4721@nyu.edu
 
@@ -18,4 +18,4 @@ module purge
 singularity exec --nv \
     --overlay /scratch/jz4721/pyenv/overlay-15GB-500K.ext3:ro \
     /scratch/work/public/singularity/cuda11.6.124-cudnn8.4.0.27-devel-ubuntu20.04.4.sif \
-    /bin/bash -c "source /ext3/env.sh; python /scratch/jz4721/SCI/EconML/DML/DML_BMI.py"
+    /bin/bash -c "source /ext3/env.sh; python /scratch/jz4721/SCI/EconML/DML/effectLinearDML.py"
