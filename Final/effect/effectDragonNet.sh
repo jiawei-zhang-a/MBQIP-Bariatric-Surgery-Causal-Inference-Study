66fa3f35d2fda03490935462658491f052d26a3f
Qishuo Yin <qy1448@princeton.edu>
2024-08-15 20:22:54 -0400

    Update and rename run_BMI.s to effectDragonNet.sh

--- Final/effect/effectDragonNet.sh
+++ Final/effect/effectDragonNet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=DNN-BMI
+#SBATCH --job-name=effectDragonNet
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=28
 #SBATCH --ntasks-per-node=1
@@ -7,8 +7,8 @@
 #SBATCH --time=79:00:00
 #SBATCH --mail-type=END
 #SBATCH --mail-user=jz4721@nyu.edu
-#SBATCH --output=BMI.out
-#SBATCH --error=BMI.err
+#SBATCH --output=effectDragonNet.out
+#SBATCH --error=effectDragonNet.err
 
 export OMP_NUM_THREADS=1
 
@@ -17,4 +17,4 @@ module purge
 singularity exec --nv \
     --overlay /scratch/jz4721/pyenv/overlay-15GB-500K.ext3:ro \
     /scratch/work/public/singularity/cuda11.6.124-cudnn8.4.0.27-devel-ubuntu20.04.4.sif \
-    /bin/bash -c "source /ext3/env.sh; python /scratch/jz4721/SCI/NewDragon/dragonnet_BMI.py"
+    /bin/bash -c "source /ext3/env.sh; python /scratch/jz4721/SCI/NewDragon/effectDragonNet.py"
