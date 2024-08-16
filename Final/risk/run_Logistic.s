e761957ca26a54ca24b29ef067b6662f4349c651
Qishuo Yin <qy1448@princeton.edu>
2024-08-15 21:32:28 -0400

    Add files via upload

---
+++ Final/risk/run_Logistic.s
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=DNN-risk
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=28
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=40G
+#SBATCH --time=30:00:00
+#SBATCH --mail-type=END
+#SBATCH --mail-user=jz4721@nyu.edu
+#SBATCH --output=Logistic.out
+#SBATCH --error=Logistic.err
+
+export OMP_NUM_THREADS=1
+
+module purge
+
+singularity exec --nv \
+    --overlay /scratch/jz4721/pyenv/overlay-15GB-500K.ext3:ro \
+    /scratch/work/public/singularity/cuda11.6.124-cudnn8.4.0.27-devel-ubuntu20.04.4.sif \
+    /bin/bash -c "source /ext3/env.sh; python /scratch/jz4721/SCI/NewDragon/dragonnet_Logistic.py"
