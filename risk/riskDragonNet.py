f0ef3a8d975e4803420aa8d0de33156075176c62
Qishuo Yin <qy1448@princeton.edu>
2025-05-19 13:50:02 -0400

    Update riskDragonNet.py

--- risk/riskDragonNet.py
+++ risk/riskDragonNet.py
@@ -9,11 +9,7 @@ np.random.seed(0)
 def main():
     print("\list \n(1)RYGB\n(2)Band\n(3)BPD-DS\n(4)SADI-S \nrelative treatment effect")
 
-    """print("\nDeath")
-    print("DragonNet")
-    print(mbqip_risk.run_mbqip_risk(PATH + "/data/mbqip/csv/Death"))
-
-    print("\nintervention")  
+    """print("\nintervention")  
     print("DragonNet")
     print(mbqip_risk.run_mbqip_risk(PATH + "/data/mbqip/csv/intervention"))"""
 
